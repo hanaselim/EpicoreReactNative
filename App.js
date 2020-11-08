52bacbbf0f55f9e29abaf015010124acfcc5abb0
hana selim <43095740+hanaselim@users.noreply.github.com>
2020-11-08 18:24:55 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, Image} from "react-native";
+import Hana from "./components/Signin.js";
+import Hana2 from "./components/Signup.js";
+
+const App = () => {
+
+  return (
+    <Hana2/>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# EpicoreReactNative
